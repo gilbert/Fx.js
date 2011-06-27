194d73f37937c8d510c9219eea34f8012f234ab3
Gilbert <gilbertbgarza@gmail.com>
2011-06-27 11:34:24 -0500

    Don't initialize every function on every $fx call. In theory this should speed up element extending.

--- fx.js
+++ fx.js
@@ -1,26 +1,5 @@
-function $fx(initElm){
-	if(initElm.nodeType && initElm.nodeType==1)
-		var elm = initElm;
-	else if(String(initElm).match(/^#([^$]+)$/i)){
-		var elm = document.getElementById(RegExp.$1+'');
-		if(!elm)
-			return null;
-	}else 
-		return null;
-		
-	if(typeof(elm._fx) != 'undefined' && elm._fx){
-		elm._fx._addSet();
-		return elm;
-	};
-	
-	elm.fxVersion = 0.1;
-	elm._fx = {};
-	elm._fx.sets = [];
-	elm._fx._currSet = 0;
-	
-	if(typeof(elm._fxTerminated) != 'undefined')
-		try{delete elm._fxTerminated}catch(err){elm._fxTerminated = null}
-	
+window.$fx = (function () {
+
 	var units = {
 		'left|top|right|bottom|width|height|margin|padding|spacing|backgroundx|backgroundy': 'px',
 		'font': 'pt',
@@ -29,86 +8,92 @@ function $fx(initElm){
 	
 	var isIE = !!navigator.userAgent.match(/MSIE/ig);
 	
-	var required = {delay: 100, step:5, unit: ''};
-	
+	var required = { delay:100, step:5, unit:'' };
+
 	var handlers = {
-		opacity: function(val, unit){
+		opacity: function (val,unit) {
 			val = parseInt(val); 
-			if(isNaN(val)){
-				if(isIE){
-					var matches = (new RegExp('alpha\\s*\\(opacity\\s*=\\s*(\\d+)\\)')).exec(elm.style.filter+'');
-					if(matches)
+			if (isNaN(val)) {
+				if (isIE) {
+					var matches = (new RegExp('alpha\\s*\\(opacity\\s*=\\s*(\\d+)\\)')).exec(this.elm.style.filter+'');
+					if (matches)
 						return parseInt(matches[1]);
 					else
 						return 1;
-				}else{
-					return Math.round((elm.style.opacity ? parseFloat(elm.style.opacity) : 1) * 100);
 				}
-			}else{
+				else {
+					return Math.round((this.elm.style.opacity ? parseFloat(this.elm.style.opacity) : 1) * 100);
+				}
+			}
+			else {
 				val = Math.min(100, Math.max(0, val));
-				if(isIE){
-					elm.style.zoom = 1;
-					elm.style.filter = 'alpha(opacity='+val+');';
-				}else{
-					elm.style.opacity = val/100;
+				if (isIE) {
+					this.elm.style.zoom = 1;
+					this.elm.style.filter = 'alpha(opacity='+val+');';
+				}
+				else {
+					this.elm.style.opacity = val/100;
 				}
 			}
 		},
-		'backgroundx': function(val, unit){
+		'backgroundx': function (val,unit) {
 			val = parseInt(val);
 			var x = 0, y = 0;
-			var matches = (new RegExp('^(-?\\d+)[^\\d\\-]+(-?\\d+)')).exec(elm.style.backgroundPosition+'');
-			if(matches){
+			var matches = (new RegExp('^(-?\\d+)[^\\d\\-]+(-?\\d+)')).exec(this.elm.style.backgroundPosition+'');
+			if (matches) {
 				x = parseInt(matches[1]);
 				y = parseInt(matches[2]);
 			}
-			if(isNaN(val))
+			if (isNaN(val))
 				return x;
-			else{
-				elm.style.backgroundPosition = val+unit+' '+y+unit;  
+			else {
+				this.elm.style.backgroundPosition = val+unit+' '+y+unit;  
 			}
 		},
-		'backgroundy': function(val, unit){
+		'backgroundy': function (val,unit) {
 			val = parseInt(val);
 			var x = 0, y = 0;
-			var matches = (new RegExp('^(-?\\d+)[^\\d\\-]+(-?\\d+)')).exec(elm.style.backgroundPosition+'');
-			if(matches){
+			var matches = (new RegExp('^(-?\\d+)[^\\d\\-]+(-?\\d+)')).exec(this.elm.style.backgroundPosition+'');
+			if (matches) {
 				x = parseInt(matches[1]);
 				y = parseInt(matches[2]);
 			}
-			if(isNaN(val))
+			if (isNaN(val))
 				return y;
-			else{
-				elm.style.backgroundPosition = x+unit+' '+val+unit;  
+			else {
+				this.elm.style.backgroundPosition = x+unit+' '+val+unit;  
 			}
 		}
 	};
 	
 	var defaults = {
-		width: function(){
+		width: function (elm) {
 			return parseInt(elm.offsetWidth)
 		},
-		height: function(){
+		height: function (elm) {
 			return parseInt(elm.offsetHeight)
 		},
-		left: function(){
+		left: function (elm) {
 			var left = 0;
-			for(var el=elm; el; el=el.offsetParent) left+=parseInt(el.offsetLeft);
+			for (var el=elm; el; el=el.offsetParent) left+=parseInt(el.offsetLeft);
 			return left;
 		},
-		top: function(){
+		top: function (elm) {
 			var top = 0;
-			for(var el=elm; el; el=el.offsetParent) top += parseInt(el.offsetTop);
+			for (var el=elm; el; el=el.offsetParent) top += parseInt(el.offsetTop);
 			return top;
 		}
 	};
 	
-	elm.fxAddSet = function(){
+	var fxFn = {};
+	// ----
+	fxFn.fxAddSet = function () {
 		this._fx._addSet();
 		return this;
 	};
 	
-	elm.fxHold = function(time, set){
+	// ----
+	fxFn.fxHold = function (time, set) {
 		if(elm._fx.sets[this._fx._currSet]._isrunning)
 			return this;
 			
@@ -117,7 +102,8 @@ function $fx(initElm){
 		return this; 
 	};
 	
-	elm.fxAdd = function(params){
+	// ----
+	fxFn.fxAdd = function (params) {
 		var currSet = this._fx._currSet;
 
 		if(this._fx.sets[currSet]._isrunning)
@@ -143,7 +129,7 @@ function $fx(initElm){
 				this._fx[params.type] = handlers[params.type];
 			else{
 				var elm = this;
-				this._fx[params.type] = function(val, unit){
+				this._fx[params.type] = function (val,unit) {
 					if(typeof(val)=='undefined')
 						return parseInt(elm.style[params.type]);
 					else
@@ -154,7 +140,7 @@ function $fx(initElm){
 		if(isNaN(params.from)){
 			if(isNaN(this._fx[params.type]()))
 				if(defaults[params.type])
-					params.from = defaults[params.type](); 
+					params.from = defaults[params.type](this);
 				else
 					params.from = 0;
 			else
@@ -166,14 +152,16 @@ function $fx(initElm){
 		return this;
 	};
 	
-	elm.fxRun = function(finalCallback, loops, loopCallback){
-		var currSet = elm._fx._currSet;
+	// ----
+	fxFn.fxRun = function(finalCallback, loops, loopCallback){
+		var currSet = this._fx._currSet;
 		
 		if(this._fx.sets[currSet]._isrunning){
 			return this;
 		}
 		
-		setTimeout(function(){
+		var elm = this;
+		setTimeout(function () {
 			if(elm._fx.sets[currSet]._isrunning)
 				return elm;
 			elm._fx.sets[currSet]._isrunning = true;
@@ -194,17 +182,20 @@ function $fx(initElm){
 		return this;
 	};
 	
-	elm.fxPause = function(status, setNum){
+	// ----
+	fxFn.fxPause = function(status, setNum){
 		this._fx.sets[!isNaN(setNum) ? setNum : this._fx._currSet]._paused = status;
 		return this;
 	};
 	
-	elm.fxStop = function(setNum){
+	// ----
+	fxFn.fxStop = function(setNum){
 		this._fx.sets[!isNaN(setNum) ? setNum : this._fx._currSet]._stoped = true;
 		return this;
 	};
 	
-	elm.fxReset = function(){
+	// ----
+	fxFn.fxReset = function(){
 			for(var i=0; i<this._fx.sets.length; i++){
 				for(var j=0; j<this._fx.sets[i]._queue.length; j++){
 					var params = this._fx.sets[i]._queue[j];
@@ -219,8 +210,9 @@ function $fx(initElm){
 				try{delete this[del[i]]}catch(err){this[del[i]] = null}
 			this._fxTerminated = true;
 	};
-	
-	elm._fx._addSet = function(){
+
+	// ----
+	var _fx_addSet = function () {
 		var currSet = this.sets.length;
 		this._currSet = currSet;
 		this.sets[currSet] = {};
@@ -236,9 +228,10 @@ function $fx(initElm){
 		
 		return this;
 	};
-	
-	elm._fx._process = function(setNum, effectNum){
-		if(!this.sets[setNum] || this.sets[setNum]._stoped || elm._fxTerminated)
+
+	// --
+	var _fx_process = function (setNum,effectNum) {
+		if(!this.sets[setNum] || this.sets[setNum]._stoped || this._fxTerminated)
 			return;
 		var ef = this.sets[setNum]._queue[effectNum];
 		var param = this[ef.type]();
@@ -251,24 +244,61 @@ function $fx(initElm){
 		}else{
 			this[ef.type](ef.to, ef.unit);
 			this.sets[setNum]._effectsDone++;
-			ef.onfinish.call(elm);
+			ef.onfinish.call(this);
 			if(this.sets[setNum]._queue.length == this.sets[setNum]._effectsDone){
 				this.sets[setNum]._effectsDone = 0;
 				this.sets[setNum]._loopsDone++;
-				this.sets[setNum]._onloop.call(elm, this.sets[setNum]._loopsDone);
+				this.sets[setNum]._onloop.call(this, this.sets[setNum]._loopsDone);
 				if(this.sets[setNum]._loopsDone < this.sets[setNum]._loops || this.sets[setNum]._loops == -1){
 					for(var i=0; i < this.sets[setNum]._queue.length; i++){
 						this[ef.type](ef.from, this.sets[setNum]._queue[i].unit);
-						this.sets[setNum]._queue[i].onstart.call(elm, this.sets[setNum]._loopsDone);
+						this.sets[setNum]._queue[i].onstart.call(this, this.sets[setNum]._loopsDone);
 						this._process(setNum, i);
 					}
 				}else{
-					this.sets[setNum]._onfinal.call(elm);
+					this.sets[setNum]._onfinal.call(this);
 				}
 			}
 		}
 	};
-	
-	elm._fx._addSet();
-	return elm;
-}
+
+
+	// the "exported" object
+	var $fx = function (initElm) {
+		if (initElm.nodeType && initElm.nodeType==1)
+			var elm = initElm;
+		else if (String(initElm).match(/^#([^$]+)$/i)) {
+			var elm = document.getElementById(RegExp.$1+'');
+			if (!elm) return null;
+		}
+		else return null;
+
+		if (typeof(elm._fx) != 'undefined' && elm._fx) {
+			elm._fx._addSet();
+			return elm;
+		};
+
+		elm.fxVersion = 0.1;
+		elm._fx = {
+			elm: elm,
+			sets: [],
+			_currSet: 0,
+			_addSet: _fx_addSet,
+			_process: _fx_process
+		};
+		
+		if (typeof(elm._fxTerminated) != 'undefined')
+			try { delete elm._fxTerminated } catch (err) { elm._fxTerminated = null }
+		
+		// extend the element
+		for (var fnName in fxFn) {
+			elm[fnName] = fxFn[fnName];
+		}
+
+		elm._fx._addSet();
+			return elm;
+	};
+
+	return $fx;
+
+})();
